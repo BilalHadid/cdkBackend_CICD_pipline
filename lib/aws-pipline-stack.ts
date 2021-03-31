bad0bda79b05e67cefc866c37323ea8c7785c677
bilalhadid <hadid0648@gmail.com>
2021-04-01 02:16:51 +0500

    Initial

--- lib/aws-pipline-stack.ts
+++ lib/aws-pipline-stack.ts
@@ -1,9 +1,19 @@
 import * as cdk from '@aws-cdk/core';
+import * as ddb from '@aws-cdk/aws-dynamodb'
 
 export class AwsPiplineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+
+    const dynamoDBTable = new ddb.Table(this,'Table',{
+      partitionKey:{
+        name:'id',
+        type: ddb.AttributeType.STRING
+      }
+    })
+
   }
 }
